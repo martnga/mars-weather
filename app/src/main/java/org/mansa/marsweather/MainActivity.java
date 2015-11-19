330e47be07b17bc94675f7acdec13f0e88c01a91
martnga <mtnjoroge@gmail.com>
2015-11-19 11:02:51 +0300

    Added Flickr Image Fetch and load

--- app/src/main/java/org/mansa/marsweather/MainActivity.java
+++ app/src/main/java/org/mansa/marsweather/MainActivity.java
@@ -1,23 +1,36 @@
 package org.mansa.marsweather;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView mTxtDegrees, mTxtWeather, mTxtError;
+    ImageView mImageView;
+    int mainColor = Color.parseColor("#FF5722");
     MarsWeather helper = MarsWeather.getInstance();
     final static String RECENT_API_ENDPOINT = "http://marsweather.ingenology.com/v1/latest/";
+    final static String
+            FLICKR_API_KEY = "[INSERT HERE YOUR API KEY]",
+            IMAGES_API_ENDPOINT = "https://api.flickr.com/services/rest/?format=json&nojsoncallback=1&sort=random&method=flickr.photos.search&" +
+                    "tags=mars,planet,rover&tag_mode=all&api_key=";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         mTxtDegrees = (TextView) findViewById(R.id.degrees);
         mTxtWeather = (TextView) findViewById(R.id.weather);
         mTxtError = (TextView) findViewById(R.id.error);
+        mImageView = (ImageView) findViewById(R.id.main_bg);
     }
 
     @Override
@@ -97,4 +111,66 @@ public class MainActivity extends AppCompatActivity {
         mTxtError.setVisibility(View.VISIBLE);
         e.printStackTrace();
     }
+
+    private void searchRandomImage() throws Exception {
+        if (FLICKR_API_KEY.equals(""))
+            throw new Exception("You didn't provide a working Flickr API!");
+
+        CustomJsonRequest request = new CustomJsonRequest
+                (Request.Method.GET, IMAGES_API_ENDPOINT+ FLICKR_API_KEY, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray images = response.getJSONObject("photos").getJSONArray("photo");
+                            int index = new Random().nextInt(images.length());
+
+                            JSONObject imageItem = images.getJSONObject(index);
+
+                            String imageUrl = "http://farm" + imageItem.getString("farm") +
+                                    ".static.flickr.com/" + imageItem.getString("server") + "/" +
+                                    imageItem.getString("id") + "_" + imageItem.getString("secret") + "_" + "c.jpg";
+
+                            // TODO: do something with *imageUrl*
+
+                        } catch (Exception e) { imageError(e); }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        imageError(error);
+                    }
+                });
+        request.setPriority(Request.Priority.LOW);
+        helper.add(request);
+    }
+
+
+
+    private void imageError(Exception e) {
+        mImageView.setBackgroundColor(mainColor);
+        e.printStackTrace();
+    }
+
+    private void loadImg(String imageUrl) {
+        // Retrieves an image specified by the URL, and displays it in the UI
+        ImageRequest request = new ImageRequest(imageUrl,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, ImageView.ScaleType.CENTER_CROP, Bitmap.Config.ARGB_8888,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        imageError(error);
+                    }
+                });
+
+        // we don't need to set the priority here;
+        // ImageRequest already comes in with
+        // priority set to LOW, that is exactly what we need.
+        helper.add(request);
+    }
+
 }
